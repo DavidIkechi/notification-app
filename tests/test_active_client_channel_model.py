5b63ef3484b1113e51ff58a80ac1bde096a17244
David Akwuru <daakwuru@gmail.com>
2023-04-08 14:15:00 +0100

    adjusted the test functions

--- tests/test_active_client_channel_model.py
+++ tests/test_active_client_channel_model.py
@@ -23,59 +23,25 @@ def test_create_active_channel(get_session):
     active_client_data = {
         "client_id": 1, "trans_channel_id": 1, "trans_config_id": 1
     }
+    #check that it is empty.
+    retrieve_active_client = ActiveChannelClientConfig.retrieve_active_channels(get_session)
+    assert len(retrieve_active_client.all()) == 0
     # create the active client
     active_client = ActiveChannelClientConfig.create_active_channel(
         get_session, active_client_data)
     get_session.add(active_client)
     get_session.commit()
-    
     # check that it was added.
     retrieve_active_client = ActiveChannelClientConfig.retrieve_active_channels(get_session)
     assert len(retrieve_active_client.all()) == 1
-    
-def test_get_active_client_by_id(get_session):
-    # seed the required tables
-    seed_transport_channel(get_session)
-    seed_notification_type(get_session)
-    seed_channel_transport(get_session)
-    seed_client(get_session)
-    seed_transport_configuration(get_session)
-    
-    active_client_data = {
-        "client_id": 1, "trans_channel_id": 1, "trans_config_id": 1
-    }
-    # create the active client
-    active_client = ActiveChannelClientConfig.create_active_channel(
-        get_session, active_client_data)
-    get_session.add(active_client)
-    get_session.commit()
     # get the client by id
     get_active_client = ActiveChannelClientConfig.get_active_channel_by_id(get_session, 1)
     assert get_active_client.client_id == 1
     assert get_active_client.trans_channel_id == 1
+    assert get_active_client.trans_config_id == 1
     
-def test_get_active_client_by_client_id(get_session):
-    # first populate the transport channel and transport type tables
-    seed_transport_channel(get_session)
-    seed_notification_type(get_session)
-    seed_channel_transport(get_session)
-    seed_client(get_session)
-    seed_transport_configuration(get_session)
     
-    active_client_data = {
-        "client_id": 1, "trans_channel_id": 1, "trans_config_id": 1
-    }
-    # create the active client
-    active_client = ActiveChannelClientConfig.create_active_channel(
-        get_session, active_client_data)
-    get_session.add(active_client)
-    get_session.commit()
-    # get the client by id
-    get_active_client = ActiveChannelClientConfig.get_active_channel_by_client_id(get_session, active_client_data['client_id'])
-    assert get_active_client is not None
-    assert len(get_active_client) == 1
-    
-def test_get_active_client_config_by_trans_channel_id(get_session):
+def test_get_active_client_by_client_trans_channel_id(get_session):
     # first populate the transport channel and transport type tables
     seed_transport_channel(get_session)
     seed_notification_type(get_session)
@@ -98,49 +64,7 @@ def test_get_active_client_config_by_trans_channel_id(get_session):
     assert get_client_config is not None
     assert get_client_config.trans_channel.slug == "email"
 
-def test_get_active_channels_by_transport(get_session):
-    # first populate the transport channel and transport type tables
-    seed_transport_channel(get_session)
-    seed_notification_type(get_session)
-    seed_channel_transport(get_session)
-    seed_client(get_session)
-    seed_transport_configuration(get_session)
-    
-    active_client_data = {
-        "client_id": 1, "trans_channel_id": 1, "trans_config_id": 1
-    }
-    # create the active client
-    active_client = ActiveChannelClientConfig.create_active_channel(
-        get_session, active_client_data)
-    get_session.add(active_client)
-    get_session.commit()
-    # get the client by id
-    get_client_config = ActiveChannelClientConfig.retrieve_active_channels_by_trans_channel(
-        get_session, active_client_data['trans_channel_id'])
-    
-    assert len(get_client_config.all()) == 1
-    
-def test_get_active_client_by_trans_config_id(get_session):
-    # first populate the transport channel and transport type tables
-    seed_transport_channel(get_session)
-    seed_notification_type(get_session)
-    seed_channel_transport(get_session)
-    seed_client(get_session)
-    seed_transport_configuration(get_session)
-    
-    active_client_data = {
-        "client_id": 1, "trans_channel_id": 1, "trans_config_id": 1
-    }
-    # create the active client
-    active_client = ActiveChannelClientConfig.create_active_channel(
-        get_session, active_client_data)
-    get_session.add(active_client)
-    get_session.commit()
-    # get the client by id
-    get_active_client = ActiveChannelClientConfig.get_active_channel_by_trans_config_id(
-        get_session, active_client_data['trans_config_id'])
-    assert get_active_client.client_id == 1
-    assert get_active_client.trans_channel_id == 1
+
     
     
     
