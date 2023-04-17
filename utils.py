8cd4a174ae5788bfa83fa7deb8c3e51f6eec1c5c
David Akwuru <daakwuru@gmail.com>
2023-04-17 07:07:27 +0100

    HMS-9-send-notification-method created the model and static methods

--- utils.py
+++ utils.py
@@ -1,3 +1,11 @@
+from enum import Enum
 # for adding utlity functions
 def get_offset(page: int, page_size: int) -> int:
     return (page - 1) * page_size
+
+# define the enum type for the status.
+class Status(Enum):
+    IN_PROGRESS = 'processing'
+    SUCCESS = 'success'
+    QUEUED = 'queue'
+    FAILED = 'failed'
